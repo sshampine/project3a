53e107f6d020d50e6022a2f322ba54ca36cdd332
sshampine <sshampine@gmail.com>
2018-01-15 17:22:08 -0600

    added concurrent clinet-server start

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const routes = require("../routes")
 const app = express();
 const PORT = process.env.PORT || 3003;
 
@@ -9,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Serve up static assets
 app.use(express.static("client/build"));
-
+app.use(routes);
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
