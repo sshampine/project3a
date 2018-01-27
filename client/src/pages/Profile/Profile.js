05585ea44decd406ef0a7b8b3681df2c5a4242b3
Greg Ladden <greg@gl-digital.com>
2018-01-27 02:11:09 -0600

    styled profile and dashboard

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -66,54 +66,129 @@ class Profile extends Component {
 
 render() {
   return (
-    <Container>
+    <Container fluid>
+      <div className="userProfile">
         <Row>
+          <Container>
             <Col size="md-3 sm-3"></Col>
             <Col size="md-9 sm-9">
-                <Col size="md-3 sm-3">
-                    <p><i class="far fa-user"></i></p>
+                <Col size="md-2 sm-2 xs-2">
+                  <p><i class="far fa-user"></i></p>
                 </Col>
-                <Col size="md-9 sm-9">
+                <Col size="md-10 sm-10">
                     <div className="userInfo">
-                        <p><strong>Name:</strong><span> {this.state.user.name}</span> </p>
-                        <p><strong>Email:</strong><span> {this.state.user.email}</span> </p>
+                      <p><strong>Name:</strong><span> {this.state.user.name}</span> </p>
+                      <p><strong>Email:</strong><span> {this.state.user.email}</span> </p>
                     </div>
                 </Col>
             </Col>
+          </Container>
         </Row>
+      </div>
         <Row>
+          <Container>
             <Col size="md-3 sm-3"></Col>
             <Col size="md-9 sm-9">
-                 <form>
-                     <h2>Choose News Outlets</h2>
+              <Col size="md-12 sm-12 xs-12">
+                <h3>Choose News Outlets</h3>
+              </Col>
+                <form>
+                  <Col size="md-9 sm-9 xs-10">
                     <Input
                         value={this.state.title}
                         onChange={this.handleInputChange}
                         name="title"
                         placeholder="Title (required)"
                     />
-                    <FormBtn onClick={this.handleFormSubmit}>
-                            Submit Outlet
-                    </FormBtn>
+                  </Col>
+                  <Col size="md-3 sm-3 xs-2">
+                      <FormBtn onClick={this.handleFormSubmit}>
+                              Submit Outlet
+                      </FormBtn>
+                  </Col>
                 </form>
-                {this.state.newsOutlet.length ? (
-              <List>
-                {this.state.newsOutlet.map(outlet => (
-                  <ListItem key={outlet._id}>
-                    <Link to={"/outlets/" + outlet._id}>
-                      <strong>
-                        {outlet.title}
-                      </strong>
-                    </Link>
-                    {/* <DeleteBtn onClick={() => this.deleteBook(book._id)} /> */}
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
-              <h3>No Results to Display</h3>
-            )}
+                {/* {this.state.newsOutlet.length ? (
+                <List>
+                  {this.state.newsOutlet.map(outlet => (
+                    <ListItem key={outlet._id}>
+                      <Link to={"/outlets/" + outlet._id}>
+                        <strong>
+                          {outlet.title}
+                        </strong>
+                      </Link>
+                      <DeleteBtn onClick={() => this.deleteBook(book._id)} />
+                    </ListItem>
+                  ))}
+                </List>
+              ) : (
+                <h3>No Results to Display</h3>
+              )} */}
+              <Col size="md-12 sm-12 xs-12">
+                <hr/>
+                  <ul className="list-inline">
+                    <li><span className="glyphicon glyphicon-remove-sign"></span>CNBC.com</li>
+                    <li><span className="glyphicon glyphicon-remove-sign"></span>CCN.com</li>
+                    
+                  </ul>
+                <hr/>
+              </Col>
             </Col>
+            </Container>
         </Row>
+        <Row>
+          <Container>
+            <Col size="md-3 sm-3"></Col>
+            <Col size="md-9 sm-9">
+              <Col size="md-12 sm-12 xs-12">
+                <h3>Choose News Topics</h3>
+              </Col>
+                <form>
+                  <Col size="md-9 sm-9 xs-10">
+                    <Input
+                        value={this.state.title}
+                        onChange={this.handleInputChange}
+                        name="title"
+                        placeholder="Title (required)"
+                    />
+                  </Col>
+                  <Col size="md-3 sm-3 xs-2">
+                      <FormBtn onClick={this.handleFormSubmit}>
+                              Submit Topic
+                      </FormBtn>
+                  </Col>
+                </form>
+                {/* {this.state.newsOutlet.length ? (
+                <List>
+                  {this.state.newsOutlet.map(outlet => (
+                    <ListItem key={outlet._id}>
+                      <Link to={"/outlets/" + outlet._id}>
+                        <strong>
+                          {outlet.title}
+                        </strong>
+                      </Link>
+                      <DeleteBtn onClick={() => this.deleteBook(book._id)} />
+                    </ListItem>
+                  ))}
+                </List>
+              ) : (
+                <h3>No Results to Display</h3>
+              )} */}
+              <Col size="md-12 sm-12 xs-12">
+                <hr/>
+                  <ul className="list-inline">
+                    <li><span className="glyphicon glyphicon-remove-sign"></span>NBA</li>
+                    <li><span className="glyphicon glyphicon-remove-sign"></span>Bitcoin</li>
+                    <li><span className="glyphicon glyphicon-remove-sign"></span>Ethereum</li>
+                  </ul>
+                <hr/>
+              </Col>
+            </Col>
+            </Container>
+        </Row>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
     </Container>
     );
   }
