06764862bfd6b054c0b4570ebd3cefa43f4e6fc7
Scott Shampine <sshampine@gmail.com>
2018-01-26 23:51:30 -0600

    deploy files

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ router.use(function(req, res) {
 })
 
 //set port, hosting services
-app.set("port", (process.env.PORT || 3000));
+app.set("port", (process.env.PORT || 3001));
 
 //start server
 app.listen(app.get("port"), () => {
