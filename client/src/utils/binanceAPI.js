c2f31ca8fc406cbb41e6959677f99d0d3a913dac
Scott Shampine <sshampine@gmail.com>
2018-01-27 08:34:48 -0600

    build files

--- client/src/utils/binanceAPI.js
+++ client/src/utils/binanceAPI.js
@@ -4,7 +4,7 @@ export default {
   allPairs: function() {
     return axios({
     	method: 'get',
-    	url: 'https://3pa.heroku.com:443/api/pairs',
+    	url: 'https://p3a.herokuapp.com:443/api/pairs',
     	auth: {
 		  username: '',
 		  password: ''
